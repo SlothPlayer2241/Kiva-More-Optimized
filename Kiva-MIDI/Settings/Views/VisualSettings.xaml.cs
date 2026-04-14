18768dd29266cb2ced54b7bd87c44c4a25677ad1
SlothPlayer2241 <slothplayer12341@gmail.com>
2026-04-14 21:36:15 +0300

    Refactor VisualSettings for improved readability

--- Kiva-MIDI/Settings/Views/VisualSettings.xaml.cs
+++ Kiva-MIDI/Settings/Views/VisualSettings.xaml.cs
@@ -1,69 +1,59 @@
-﻿using Kiva.UI;
+using Kiva.UI;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Kiva.Settings.Views
 {
-    /// <summary>
-    /// Interaction logic for VisualSettings.xaml
-    /// </summary>
     public partial class VisualSettings : UserControl
     {
         private KivaSettings settings;
+        private readonly Brush selectBrush = new SolidColorBrush(Color.FromArgb(50, 255, 255, 255));
 
         public KivaSettings Settings
         {
-            get => settings; set
+            get => settings;
+            set
             {
                 settings = value;
                 SetValues();
             }
         }
 
-        SolidColorBrush selectBrush = new SolidColorBrush(Color.FromArgb(50, 255, 255, 255));
-
         public VisualSettings()
         {
             InitializeComponent();
         }
 
-        void SetValues()
-        {
-            var range = settings.General.KeyRange;
-            if (range == KeyRangeTypes.Key88) key88Range.IsChecked = true;
-            if (range == KeyRangeTypes.Key128) key128Range.IsChecked = true;
-            if (range == KeyRangeTypes.Key256) key256Range.IsChecked = true;
-            if (range == KeyRangeTypes.KeyMIDI) midiRange.IsChecked = true;
-            if (range == KeyRangeTypes.KeyDynamic) dynamicRange.IsChecked = true;
-            if (range == KeyRangeTypes.Custom) customRange.IsChecked = true;
+        private bool Ready() => IsInitialized && settings != null;
 
-            var style = settings.General.KeyboardStyle;
-            if (style == KeyboardStyle.Big) bigKeyboard.IsChecked = true;
-            if (style == KeyboardStyle.Small) smallKeyboard.IsChecked = true;
-            if (style == KeyboardStyle.None) noKeyboard.IsChecked = true;
+        private void SetValues()
+        {
+            switch (settings.General.KeyRange)
+            {
+                case KeyRangeTypes.Key88: key88Range.IsChecked = true; break;
+                case KeyRangeTypes.Key128: key128Range.IsChecked = true; break;
+                case KeyRangeTypes.Key256: key256Range.IsChecked = true; break;
+                case KeyRangeTypes.KeyMIDI: midiRange.IsChecked = true; break;
+                case KeyRangeTypes.KeyDynamic: dynamicRange.IsChecked = true; break;
+                case KeyRangeTypes.Custom: customRange.IsChecked = true; break;
+            }
+
+            switch (settings.General.KeyboardStyle)
+            {
+                case KeyboardStyle.Big: bigKeyboard.IsChecked = true; break;
+                case KeyboardStyle.Small: smallKeyboard.IsChecked = true; break;
+                case KeyboardStyle.None: noKeyboard.IsChecked = true; break;
+            }
 
             fpsLock.Value = settings.General.FPSLock;
-            //compatibilityFps.IsChecked = settings.General.CompatibilityFPS;
-
-            var first = settings.General.CustomFirstKey;
-            var last = settings.General.CustomLastKey;
-
-            firstKey.Value = first;
-            lastKey.Value = last;
+            firstKey.Value = settings.General.CustomFirstKey;
+            lastKey.Value = settings.General.CustomLastKey;
 
             syncFps.IsChecked = settings.General.SyncFPS;
             fpsLock.IsEnabled = !syncFps.IsChecked;
@@ -73,104 +63,105 @@ namespace Kiva.Settings.Views
             SetPalettes();
         }
 
-        void SetPalettes()
+        private void SetPalettes()
         {
-            foreach (var p in settings.PaletteSettings.Palettes.Keys)
+            palettesPanel.Children.Clear();
+
+            foreach (var name in settings.PaletteSettings.Palettes.Keys)
             {
-                var item = new Grid()
+                var item = new Grid { Tag = name };
+
+                item.Children.Add(new RippleEffectDecorator
                 {
-                    Tag = p,
-                    Background = Brushes.Transparent
-                };
-                item.Children.Add(
-                    new RippleEffectDecorator()
-                    {
-                        Content = new Label
-                        {
-                            Content = p
-                        }
-                    }
-                );
-                item.PreviewMouseDown += (s, e) =>
+                    Content = new Label { Content = name }
+                });
+
+                item.PreviewMouseDown += (_, __) =>
                 {
-                    if (settings.General.PaletteName == p) return;
-                    foreach (var i in palettesPanel.Children.Cast<Grid>()) i.Background = Brushes.Transparent;
-                    settings.General.PaletteName = p;
+                    if (settings.General.PaletteName == name) return;
+
+                    foreach (Grid g in palettesPanel.Children)
+                        g.Background = Brushes.Transparent;
+
+                    settings.General.PaletteName = name;
                     item.Background = selectBrush;
                 };
-                if (p == settings.General.PaletteName)
+
+                if (name == settings.General.PaletteName)
                     item.Background = selectBrush;
+
                 palettesPanel.Children.Add(item);
             }
         }
 
         private void RangeChanged(object sender, RoutedEventArgs e)
         {
-            if (!IsInitialized) return;
+            if (!Ready()) return;
+
             if (sender == key88Range) settings.General.KeyRange = KeyRangeTypes.Key88;
-            if (sender == key128Range) settings.General.KeyRange = KeyRangeTypes.Key128;
-            if (sender == key256Range) settings.General.KeyRange = KeyRangeTypes.Key256;
-            if (sender == midiRange) settings.General.KeyRange = KeyRangeTypes.KeyMIDI;
-            if (sender == dynamicRange) settings.General.KeyRange = KeyRangeTypes.KeyDynamic;
-            if (sender == customRange) settings.General.KeyRange = KeyRangeTypes.Custom;
+            else if (sender == key128Range) settings.General.KeyRange = KeyRangeTypes.Key128;
+            else if (sender == key256Range) settings.General.KeyRange = KeyRangeTypes.Key256;
+            else if (sender == midiRange) settings.General.KeyRange = KeyRangeTypes.KeyMIDI;
+            else if (sender == dynamicRange) settings.General.KeyRange = KeyRangeTypes.KeyDynamic;
+            else if (sender == customRange) settings.General.KeyRange = KeyRangeTypes.Custom;
         }
 
         private void KBStyleChanged(object sender, RoutedEventArgs e)
         {
-            if (!IsInitialized) return;
+            if (!Ready()) return;
+
             if (sender == noKeyboard) settings.General.KeyboardStyle = KeyboardStyle.None;
-            if (sender == smallKeyboard) settings.General.KeyboardStyle = KeyboardStyle.Small;
-            if (sender == bigKeyboard) settings.General.KeyboardStyle = KeyboardStyle.Big;
+            else if (sender == smallKeyboard) settings.General.KeyboardStyle = KeyboardStyle.Small;
+            else if (sender == bigKeyboard) settings.General.KeyboardStyle = KeyboardStyle.Big;
         }
 
-        private void FirstKey_ValueChanged(object sender, RoutedPropertyChangedEventArgs<decimal> e)
+        private void FirstKey_ValueChanged(object s, RoutedPropertyChangedEventArgs<decimal> e)
         {
-            if (!IsInitialized) return;
+            if (!Ready()) return;
             settings.General.CustomFirstKey = (int)firstKey.Value;
         }
 
-        private void LastKey_ValueChanged(object sender, RoutedPropertyChangedEventArgs<decimal> e)
+        private void LastKey_ValueChanged(object s, RoutedPropertyChangedEventArgs<decimal> e)
         {
-            if (!IsInitialized) return;
+            if (!Ready()) return;
             settings.General.CustomLastKey = (int)lastKey.Value;
         }
 
-        private void FpsLock_ValueChanged(object sender, RoutedPropertyChangedEventArgs<decimal> e)
+        private void FpsLock_ValueChanged(object s, RoutedPropertyChangedEventArgs<decimal> e)
         {
-            if (!IsInitialized) return;
+            if (!Ready()) return;
             settings.General.FPSLock = (int)fpsLock.Value;
         }
 
-        private void CompatibilityFps_CheckToggled(object sender, RoutedPropertyChangedEventArgs<bool> e)
+        private void OpenPaletteFolder_PreviewMouseDown(object s, MouseButtonEventArgs e)
         {
-            if (!IsInitialized) return;
-            //settings.General.CompatibilityFPS = compatibilityFps.IsChecked;
+            var path = System.IO.Path.Combine(
+                System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location),
+                "Palettes");
+
+            Process.Start("explorer.exe", path);
         }
 
-        private void OpenPaletteFolder_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        private void ReloadPalettes_PreviewMouseDown(object s, MouseButtonEventArgs e)
         {
-            Process.Start("explorer.exe", System.IO.Path.Combine(System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "Palettes\\"));
-        }
-
-        private void ReloadPalettes_PreviewMouseDown(object sender, MouseButtonEventArgs e)
-        {
-            palettesPanel.Children.Clear();
             settings.PaletteSettings.Reload();
-            if (!settings.PaletteSettings.Palettes.Keys.Contains(settings.General.PaletteName))
+
+            if (!settings.PaletteSettings.Palettes.ContainsKey(settings.General.PaletteName))
                 settings.General.PaletteName = settings.PaletteSettings.Palettes.Keys.First();
 
             SetPalettes();
         }
 
-        private void RandomisePaletteOrder_CheckToggled(object sender, RoutedPropertyChangedEventArgs<bool> e)
+        private void RandomisePaletteOrder_CheckToggled(object s, RoutedPropertyChangedEventArgs<bool> e)
         {
-            if (!IsInitialized) return;
+            if (!Ready()) return;
             settings.General.PaletteRandomized = randomisePaletteOrder.IsChecked;
         }
 
-        private void syncFps_CheckToggled(object sender, RoutedPropertyChangedEventArgs<bool> e)
+        private void syncFps_CheckToggled(object s, RoutedPropertyChangedEventArgs<bool> e)
         {
-            if (!IsInitialized) return;
+            if (!Ready()) return;
+
             settings.General.SyncFPS = syncFps.IsChecked;
             fpsLock.IsEnabled = !syncFps.IsChecked;
         }
